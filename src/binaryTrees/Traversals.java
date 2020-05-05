b94131c7792ebf236a605360c3107798d61b2c1b
rahul7t <rahulthosar07@gmail.com>
2020-05-05 00:51:40 -0400

    Added preorder recursive and iterative traversal

--- src/binaryTrees/Traversals.java
+++ src/binaryTrees/Traversals.java
@@ -1,7 +1,6 @@
 package binaryTrees;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 public class Traversals {
 
@@ -12,16 +11,65 @@ public class Traversals {
         TreeNode root = new TreeNode(1);
         root.right = new TreeNode(2);
         root.right.left = new TreeNode(3);
-        obj.inorder(root);
-
+        //obj.inorderRecursive(root);
+        //obj.inorderIterative(root);
+        obj.preOrderRecursive(root);
+        //obj.preOrderIterative(root);
     }
 
-    public void inorder(TreeNode root){
+    public void inorderRecursive(TreeNode root){
         if (root == null)
              return;
-        inorder(root.left);
+        inorderRecursive(root.left);
         System.out.println(": " + root.val);
-        inorder(root.right);
+        inorderRecursive(root.right);
     }
+
+    public void inorderIterative(TreeNode root){
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+
+            while (curr!=null) {
+                stack.push(curr);
+                curr=curr.left;
+            }
+            curr = stack.pop();
+            System.out.println(": "+curr.val);
+            curr=curr.right;
+        }
+    }
+
+
+    public void preOrderRecursive(TreeNode root){
+        if (root==null){
+            return;
+        }
+        System.out.println(":"+ root.val);
+        preOrderRecursive(root.left);
+        preOrderRecursive(root.right);
+    }
+
+    public void preOrderIterative(TreeNode root){
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            System.out.println(": "+node.val);
+            if(node.right!=null){
+                stack.push(node.right);
+            }
+            if(node.left!=null){
+                stack.push(node.left);
+            }
+        }
+        return;
+    }
+
+
 
 }
